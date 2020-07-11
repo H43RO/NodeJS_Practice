37a03de9d67f8beb3f1bfbe43b37c4087d5a1db7
H43RO <h2is1234@naver.com>
2020-07-11 14:57:04 +0900

    Implementation Loading data list

--- main.js
+++ main.js
@@ -9,56 +9,71 @@ var app = http.createServer(function(request, response) {
 
     if (pathName === '/') {
         if (queryData.id === undefined) {
-            fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, data) {
+            fs.readdir('./data', function(err, filelist) {
                 var title = 'Welcome';
                 var description = 'Hello, Node.js!';
+
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                    i++;
+                }
+                list = list + '</ul>';
+
                 var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                <ul>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ul>
-                <h2>${title}</h2>
-                <p> ${description}</p>
-                </body>
-                </html>
-                `;
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        ${list}
+                        <h2>${title}</h2>
+                        <p> ${description}</p>
+                    </body>
+                    </html>
+                    `;
                 response.writeHead(200);
                 response.end(template);
             });
+
         } else {
-            fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, data) {
-                var title = queryData.id;
-                var description = data;
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                <ul>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ul>
-                <h2>${title}</h2>
-                <p> ${description}</p>
-                </body>
-                </html>
-                `;
-                response.writeHead(200);
-                response.end(template);
+            fs.readdir('./data', function(err, filelist) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js!';
+
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                    i++;
+                }
+                list = list + '</ul>';
+
+                fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, data) {
+                    var title = queryData.id;
+                    var description = data;
+                    var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        ${list}
+                        <h2>${title}</h2>
+                        <p> ${description}</p>
+                    </body>
+                    </html>
+                    `;
+                    response.writeHead(200);
+                    response.end(template);
+                });
             });
         }
     } else {
