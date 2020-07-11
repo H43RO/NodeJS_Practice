81e29c1decd55f1a5c822445c474002e95269628
H43RO <h2is1234@naver.com>
2020-07-11 14:02:27 +0900

    Implementation Error page

--- main.js
+++ main.js
@@ -6,40 +6,39 @@ var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id;
-    console.log(queryData.id);
-    if (_url == '/') {
-        title = 'Welcome!';
+    var pathName = url.parse(_url, true).pathname;
+
+    if (pathName === '/') {
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, data) {
+            var description = data;
+
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p> ${description}</p>
+            </body>
+            </html>
+            `;
+
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else {
+        response.writeHead(400);
+        response.end('404 not found');
     }
-    if (_url == '/favicon.ico') {
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-
-    fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, data) {
-        var description = data;
-
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p> ${description}</p>
-        </body>
-        </html>
-        `;
-        response.end(template);
-    });
-
 
 });
 app.listen(3000);
